0f071783b0d113f3ce9662975bf0746f03e7dfc7
Andrew <nyhusandrew@gmail.com>
2024-06-27 14:31:59 -0400

    create server container, no actual server yet

---
+++ api_server/go.mod
@@ -0,0 +1,3 @@
+module github.com/andrewnyhus/RepoVault
+
+go 1.22.4
---
+++ api_server/server.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	// TODO start server
+	for true {
+		fmt.Println("HI")
+		time.Sleep(3 * time.Second)
+	}
+}
